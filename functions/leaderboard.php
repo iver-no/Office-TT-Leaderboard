4a590254d20667cb669a99f64c8a3d7586c90265
Coow <eirik_skarding@hotmail.no>
2019-08-19 17:20:04 +0200

    Removed showing new people from the leaderboard

--- functions/leaderboard.php
+++ functions/leaderboard.php
@@ -5,7 +5,7 @@
 
         $link = mysqli_connect($ip,$username,$password,$database);
         mysqli_set_charset($link, "utf8");
-        $query = "SELECT * FROM elo ORDER BY ELO DESC, Wins DESC";
+        $query = "SELECT FirstName, LastName, ELO, Wins, Losses FROM elo WHERE Wins >= 1 OR Losses >= 1 ORDER BY ELO DESC, Wins DESC";
 
         $result = mysqli_query($link, $query);
         echo "<div id='leaderboard'><table align='center' border='0' cellspacing='0'>";#<tr><th>Name</th><th>ELO</th><th>Wins</th><th>Losses</th><th>WinRatio</th></tr>";
